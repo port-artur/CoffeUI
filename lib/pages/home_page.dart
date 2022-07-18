a88e9c81d2dec84fe19b85c0eeeb5523d7200a95
MadIrishman <a25leksandr@mail.ru>
2022-07-18 23:38:44 +0600

    Coffe
    Coffe

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Icon(Icons.menu),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: Icon(Icons.person),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: '',
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          //Fin the best coffe for you
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text(
+              "Fin the best coffe for you",
+              style: TextStyle(
+                fontSize: 36,
+              ),
+            ),
+          )
+          //Search Bar
+
+          //horizontal listviewof coffe tiles
+        ],
+      ),
+    );
+  }
+}
