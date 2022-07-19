5d95b6e62cd99ee470b15c6dde6d8c952e730f92
MadIrishman <a25leksandr@mail.ru>
2022-07-20 00:50:26 +0600

    Part 1

---
+++ android — копия/app/src/main/kotlin/com/example/coffe_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coffe_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib — копия/pages/home_page.dart
@@ -0,0 +1,89 @@
+import 'package:coffe_project/util/coffee_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Icon(
+          Icons.menu,
+          color: Colors.orange,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: Icon(Icons.person),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: '',
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          //Fin the best coffe for you
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text(
+              "Fin the best coffe for you",
+              style: GoogleFonts.bebasNeue(
+                fontSize: 56,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          //Search Bar
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Find your coffe...',
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade600)),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade600)),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          //horizontal listviewof coffe tiles
+          Expanded(
+              child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [CoffeeTile()],
+          ))
+        ],
+      ),
+    );
+  }
+}
