1f0bc2d4c5de8748577fa4ac39cb9d81c9ec0f95
MadIrishman <a25leksandr@mail.ru>
2022-07-20 00:52:09 +0600

    Part1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:coffe_project/util/coffee_tile.dart';
+import 'package:coffe_project/util/coffee_type.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -10,6 +11,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+// list of coffee types
+  final List coffeeType = [
+    // coffee type, isSelected
+    [
+      'Cappucino',
+      true,
+    ],
+    [
+      'Latte',
+      false,
+    ],
+    [
+      'Black',
+      false,
+    ],
+    [
+      'Tea',
+      false,
+    ],
+  ];
+
+// user tapped on coffee types
+
+  void coffeeTypeSelected(int index) {
+    setState(() {
+      for (int i = 0; i < coffeeType.length; i++) {
+        coffeeType[i][1] = false;
+      }
+      coffeeType[index][1] = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +52,6 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.transparent,
         leading: Icon(
           Icons.menu,
-          color: Colors.orange,
         ),
         actions: [
           Padding(
@@ -76,11 +108,47 @@ class _HomePageState extends State<HomePage> {
           SizedBox(
             height: 25,
           ),
+
+          //horizontal
+          Container(
+              height: 50,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: coffeeType.length,
+                  itemBuilder: (context, index) {
+                    return CoffeeType(
+                      coffeeType: coffeeType[index][0],
+                      isSelected: coffeeType[index][1],
+                      onTap: () {
+                        coffeeTypeSelected(index);
+                      },
+                    );
+                  })),
+
           //horizontal listviewof coffe tiles
           Expanded(
               child: ListView(
             scrollDirection: Axis.horizontal,
-            children: [CoffeeTile()],
+            children: [
+              CoffeeTile(
+                coffeeImagePatch: 'lib/images/latte.jpg',
+                coffeeName: 'Latte',
+                coffeeDescription: 'Coffee drink based on milk.',
+                coffeePrice: '3.80',
+              ),
+              CoffeeTile(
+                coffeeImagePatch: 'lib/images/cappucino.png',
+                coffeeName: 'Cappucino',
+                coffeeDescription: 'Espresso-based coffee drink of Italian cuisine.',
+                coffeePrice: '4.50',
+              ),
+              CoffeeTile(
+                coffeeImagePatch: 'lib/images/milk.jpg',
+                coffeeName: 'Milk',
+                coffeeDescription: 'Natural milk, fat content 6%.',
+                coffeePrice: '2.50',
+              ),
+            ],
           ))
         ],
       ),
--- lib/util/coffee_tile.dart
+++ lib/util/coffee_tile.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 
 class CoffeeTile extends StatelessWidget {
-  const CoffeeTile({Key? key}) : super(key: key);
+
+  final String coffeeImagePatch;
+  final String coffeeName;
+  final String coffeePrice;
+  final String coffeeDescription;
+
+  CoffeeTile({
+    required this.coffeeImagePatch, 
+    required this.coffeeName, 
+    required this.coffeePrice,
+    required this.coffeeDescription, 
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 25.0, bottom: 25),
       child: Container(
+         width: 200,
         padding: EdgeInsets.all(12),
-        width: 200,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
           color: Colors.black54,
@@ -20,29 +31,46 @@ class CoffeeTile extends StatelessWidget {
             //coffee image
             ClipRRect(
                 borderRadius: BorderRadius.circular(12),
-                child: Image.asset('lib/images/latte.jpg')),
+                child: Image.asset(coffeeImagePatch, width: 200, height: 150, fit: BoxFit.fill)),
 
             // coffee name
-            // Padding(
-            //   padding:
-            //       const EdgeInsets.symmetric(vertical: 12.0, horizontal: 8),
-            //   child: Column(
-            //     crossAxisAlignment: CrossAxisAlignment.start,
-            //     children: [
-            //       Text(
-            //         'Latte',
-            //         style: TextStyle(fontSize: 20),
-            //       ),
-            //       SizedBox(
-            //         height: 4,
-            //       ),
-            //       Text(
-            //         'With Almound Milk',
-            //         style: TextStyle(color: Colors.grey[700]),
-            //       )
-            //     ],
-            //   ),
-            // )
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 12.0, horizontal: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    coffeeName,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    coffeeDescription,
+                    style: TextStyle(color: Colors.grey[700]),
+                  )
+                ],
+              ),
+            ),
+            //price
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('\$' + coffeePrice),
+                  Container(
+                      padding: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Icon(Icons.add))
+                ],
+              ),
+            )
           ],
         ),
       ),
